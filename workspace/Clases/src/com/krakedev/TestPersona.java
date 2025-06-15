04deee6ffb52ad0b4dd37cf33d83cc4269d03202
joaquinbowen <bowenjoaquinsd@gmail.com>
2025-06-15 16:51:31 -0500

    clases persona

---
+++ workspace/Clases/src/com/krakedev/TestPersona.java
@@ -0,0 +1,35 @@
+package com.krakedev;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		// Esto es un comentario
+		Persona p; // 1.-Declaro una variable p de tipo Persona
+		p = new Persona(); // 2.-Instanciar un objeto persona, referenciarlo con p.
+		Persona p2 = new Persona();
+		
+		// 3.-Acceder a los atributos.
+		System.out.println("nombre: " + p.nombre);
+		System.out.println("edad: " + p.edad);
+		System.out.println("estatura: " + p.estatura);
+		
+		// 4.- Modificar los atributos
+		p.nombre = "Mario";
+		p.edad = 45;
+		p.estatura = 1.59;
+		
+		System.out.println("--------------------------------------");
+		// 5.-Acceder a los atributos.
+		System.out.println("nombre: " + p.nombre);
+		System.out.println("edad: " + p.edad);
+		System.out.println("estatura: " + p.estatura);
+		
+		p2.nombre = "angelina";
+		
+		System.out.println("******************");
+		System.out.println("p.nombre: "+p.nombre);
+		System.out.println("p2.nombre: "+p2.nombre);
+
+	}
+
+}
