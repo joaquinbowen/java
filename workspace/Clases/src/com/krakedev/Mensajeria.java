9a1036d1b0bde2f6de878bf22e9d0dfa058a9fe8
joaquinbowen <bowenjoaquinsd@gmail.com>
2025-06-15 19:28:03 -0500

    funcion multiplicar

---
+++ workspace/Clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,10 @@
+package com.krakedev;
+
+public class Mensajeria {
+	public  void saludar() {
+		System.out.println("Hola Humanos");
+	}
+	public void saludarAmigo(String nombre,String apellido) {
+		System.out.println("Hola "+nombre+" "+ apellido);
+	}
+}
