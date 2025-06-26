e05f151556cfcc08a661f242bb3cd0b3b72df8b8
joaquinbowen <bowenjoaquinsd@gmail.com>
2025-06-26 13:23:42 -0500

    reto 30 video maven junit

---
+++ workspace/ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,12 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	
+	public int sumar(int v1,int v2) {
+		return v1+v2;
+	}
+	
+	public int restar(int v1,int v2) {
+		return v1-v2;
+	}
+}
---
+++ workspace/ejemplomaven/src/main/java/com/krakedev/ejemplomaven/TestAdmin.java
@@ -0,0 +1,8 @@
+package com.krakedev.ejemplomaven;
+
+public class TestAdmin {
+	public static void main(String[] args) {
+		Admin admin=new Admin();
+		admin.agregar();
+	}
+}
